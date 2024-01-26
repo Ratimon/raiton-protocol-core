6603eb6dad87a935dd0e0886020f886d909a0cfa
Ratimon <ratixoxo@gmail.com>
2024-01-26 10:44:57 +0700

    Refactor (./contracts): Separating between unit and invariant test suites

---
+++ contracts/test/invariant/PoolsCounterBalancer.invariants.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.20;
+
+import {Test} from "@forge-std/Test.sol";
+import {StdInvariant} from "@forge-std/StdInvariant.sol";
+
+import {BalanceAccountHarness} from "@test/harness/BalanceAccount.harness.t.sol";
+import {CoreHarness} from "@test/harness/Core.harness.t.sol";
+
+contract PoolsCounterBalancerInvariants is StdInvariant, Test {
+
+
+}
+
